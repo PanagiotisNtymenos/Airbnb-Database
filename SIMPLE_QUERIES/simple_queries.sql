8053f3fe63da569762ecbf2e623ae227349875f9
PanagiotisNtymenos <panagiwths.nty@gmail.com>
2020-04-15 14:47:11 +0300

    Question Marks

--- SIMPLE_QUERIES/simple_queries.sql
+++ SIMPLE_QUERIES/simple_queries.sql
@@ -6,7 +6,7 @@ Output: 324 rows
 SELECT DISTINCT Listings.name, Listings.zipcode, Listings.latitude, Listings.longitude FROM Listings
 INNER JOIN Calendar
 ON Listings.id = Calendar.listing_id
-WHERE Calendar.date BETWEEN '20200401' AND '20200405' AND Listings.neighbourhood_cleansed = 'ΚΥΨΕΛΗ'
+WHERE Calendar.date BETWEEN '20200401' AND '20200405' AND Listings.neighbourhood_cleansed = 'ΚΥΨΕΛΗ';
 
 
 
@@ -19,7 +19,7 @@ SELECT Listings.name, Listings.price, Reviews.reviewer_name, Reviews.comments FR
 INNER JOIN Reviews
 ON Listings.id = Reviews.listing_id
 WHERE Listings.number_of_reviews <= 10 AND LENGTH(Listings.price) <= 6 AND CAST((RIGHT(Listings.price, LENGTH(Listings.price) - 1 )) as DECIMAL(9,2)) BETWEEN 20 AND 50
-ORDER BY Listings.price ASC
+ORDER BY Listings.price ASC;
 
 
 
@@ -33,7 +33,7 @@ INNER JOIN Calendar
 ON Listings.id = Calendar.listing_id
 WHERE CAST(Listings.review_scores_rating AS INT) >= 90 AND Calendar.minimum_nights >= 5 AND Listings.number_of_reviews >= 100
 ORDER BY Listings.price ASC
-LIMIT 10
+LIMIT 10;
 
 
 
@@ -47,7 +47,7 @@ LEFT JOIN Reviews
 ON Reviews.listing_id = Listings.id
 RIGHT JOIN Neighbourhoods
 ON Neighbourhoods.neighbourhood = Listings.neighbourhood_cleansed
-GROUP BY Neighbourhoods.neighbourhood
+GROUP BY Neighbourhoods.neighbourhood;
 
 
 
@@ -59,7 +59,7 @@ Output: 45 rows
 SELECT Neighbourhoods.neighbourhood, COUNT(Listings.id) AS houses, ROUND( AVG( CAST( Listings.review_scores_rating AS NUMERIC(9, 2))), 2) AS MO FROM Listings
 LEFT JOIN Neighbourhoods
 ON Neighbourhoods.neighbourhood = Listings.neighbourhood_cleansed
-GROUP BY Neighbourhoods.neighbourhood
+GROUP BY Neighbourhoods.neighbourhood;
 
 
 
@@ -74,7 +74,7 @@ ON Calendar.listing_id = Listings.id
 JOIN Reviews
 ON Reviews.listing_id = Listings.id
 WHERE CAST(Calendar.date AS VARCHAR(10)) LIKE '2020%'
-GROUP BY Listings.id
+GROUP BY Listings.id;
 
 
 
@@ -88,7 +88,7 @@ JOIN Listings
 ON Reviews.listing_id = Listings.id
 JOIN Neighbourhoods
 ON Neighbourhoods.neighbourhood = Listings.neighbourhood_cleansed
-WHERE CAST(Reviews.date AS VARCHAR(10)) LIKE '2018%' AND Listings.neighbourhood_cleansed = 'ΠΑΓΚΡΑΤΙ'
+WHERE CAST(Reviews.date AS VARCHAR(10)) LIKE '2018%' AND Listings.neighbourhood_cleansed = 'ΠΑΓΚΡΑΤΙ';
 
 
 
@@ -107,7 +107,7 @@ JOIN (SELECT Reviews.reviewer_id AS rev_id, COUNT(Reviews.reviewer_id) FROM Revi
 ON Reviews.reviewer_id = rev_id
 JOIN Listings
 ON Listings.id = Reviews.listing_id
-ORDER BY Reviews.date
+ORDER BY Reviews.date;
 
 
 
@@ -118,7 +118,7 @@ Output: 83 rows
 
 SELECT DISTINCT Listings.host_id, Listings.host_name, Listings.host_since, COUNT(Listings.id) FROM Listings
 WHERE CAST(Listings.host_since AS VARCHAR(10)) < '2012%' AND Listings.review_scores_rating >= '90'
-GROUP BY Listings.host_id, Listings.host_name, Listings.host_since
+GROUP BY Listings.host_id, Listings.host_name, Listings.host_since;
 
 
 
@@ -129,7 +129,7 @@ Output: 114 rows
 
 SELECT Listings.id, Listings.name, Listings.price, Listings.review_scores_rating FROM Listings
 WHERE LENGTH(Listings.price) <= 6 AND CAST((RIGHT(Listings.price, LENGTH(Listings.price) - 1 )) as DECIMAL(9,2)) <= 50 AND Listings.review_scores_rating >= '90' AND Listings.neighbourhood_cleansed = 'ΠΕΤΡΑΛΩΝΑ'
-ORDER BY Listings.id
+ORDER BY Listings.id;
 
 
 
@@ -141,4 +141,4 @@ Output: 47 rows
 SELECT Listings.id, Listings.name, Listings.price FROM Listings
 GROUP BY Listings.id
 HAVING Listings.price = (SELECT MIN(Listings.price) FROM Listings
-							WHERE Listings.price > '$0.00')	
+							WHERE Listings.price > '$0.00');	
