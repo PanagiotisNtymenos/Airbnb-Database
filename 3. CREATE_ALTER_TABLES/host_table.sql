f7f9742f6f3b6f0ce3075da5d6ee097ad4b1da01
PanagiotisNtymenos <panagiwths.nty@gmail.com>
2020-04-24 12:43:28 +0300

    Regrouped!!

---
+++ 3. CREATE_ALTER_TABLES/host_table.sql
@@ -0,0 +1,21 @@
+-- Execute to backup
+CREATE TABLE CalendarB AS 
+TABLE Calendar;
+
+CREATE TABLE ListingsB AS 
+TABLE Listings;
+
+CREATE TABLE NeighbourhoodsB AS 
+TABLE Neighbourhoods;
+
+CREATE TABLE ReviewsB AS 
+TABLE Reviews;
+
+-- Execute to drop backup
+DROP TABLE CalendarB;
+
+DROP TABLE ListingsB;
+
+DROP TABLE NeighbourhoodsB;
+
+DROP TABLE ReviewsB;
