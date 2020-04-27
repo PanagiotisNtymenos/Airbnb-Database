d8f3b11380a695b8780a4717c0fd6f4eada2950b
PanagiotisNtymenos <panagiwths.nty@gmail.com>
2020-04-27 23:16:44 +0300

    Done!

--- 3.CREATE_ALTER_TABLES/SQL/sq.sql
+++
@@ -1,9 +0,0 @@
-	UPDATE Cal
-	SET price = NODOLLAR.pr,
-		adjusted = NODOLLAR.ad
-	FROM (SELECT listing_id, date,
-		  REPLACE(price, '$', '') AS pr, 
-		  REPLACE(adjusted, '$', '') AS ad FROM Calendarb) AS NODOLLAR
-	WHERE Cal.listing_id = NODOLLAR.listing_id AND Cal.date = NODOLLAR.date;
-																								   
- 
